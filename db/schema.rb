5669a58801964d5f25848123ccaf5ba3c33f234d
Rebecca Strauss <rstrauss127@gmail.com>
2019-04-13 22:03:07 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140212205242) do
+ActiveRecord::Schema.define(version: 20190413220235) do
 
-  create_table "rides", force: :cascade do |t|
+  create_table "passengers", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "rides", force: :cascade do |t|
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.integer  "passenger_id"
+    t.integer  "taxi_id"
   end
 
   create_table "taxis", force: :cascade do |t|
